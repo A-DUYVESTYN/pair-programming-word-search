d01c07ed5fe6b24f1ca047739aa1d7c1a003dc30
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-07-07 00:43:58 +0000

    finished paired programming seesion with Tianqi Wei

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,39 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  
+  
+  
+  
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'N', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'K', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'], 
+      ['H', 'M', 'J', 'F', 'E', 'V', 'R', 'G'], // FRANK
+      ['W', 'H', 'C', 'R', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'A', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'E', 'N', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'D', 'K', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'A', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is present over two columns", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'N', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'K', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'], 
+      ['H', 'M', 'J', 'F', 'E', 'V', 'R', 'G'], 
+      ['W', 'H', 'C', 'R', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'F', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'F', 'H', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'R', 'H', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'A', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isFalse(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
+
+    let result = false
+
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) result = true
+        
     }
+
+    for (i in letters[0]) {
+        const verticalJoin = []
+
+        for (j in letters) {
+            verticalJoin.push(letters[j][i])
+            
+        }
+        // console.log(verticalJoin.join(''))
+        if (verticalJoin.join('').includes(word)) result = true
+    }
+
+    return result
+
 }
+
+//  TEST CASE
+// console.log(wordSearch([
+//     ['A', 'W', 'C', 'N', 'Q', 'U', 'A', 'L'],
+//     ['S', 'E', 'I', 'K', 'F', 'E', 'L', 'D'],
+//     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'], 
+//     ['H', 'M', 'J', 'F', 'E', 'V', 'R', 'G'], // FRANK
+//     ['W', 'H', 'C', 'R', 'Y', 'E', 'R', 'L'],
+//     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//     ['U', 'B', 'E', 'N', 'A', 'P', 'A', 'I'],
+//     ['O', 'D', 'D', 'K', 'K', 'U', 'A', 'S'],
+//     ['E', 'Z', 'A', 'F', 'Q', 'U', 'A', 'L'],
+//   ], 'FRANK'))
 
 module.exports = wordSearch
